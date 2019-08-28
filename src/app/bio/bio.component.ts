5aac05c1db54882f45d6c0f4d26ecdf6bc8c8732
DominikLipinski <maboonga@gmail.com>
2019-08-28 17:04:15 +0200

    current job

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -9,6 +9,26 @@ export class BioComponent implements OnInit {
 
   constructor() { }
 
+  currentVisible: boolean = false;
+
+  jobs = {
+    current: {
+      title: 'Frontend (fullstack) Developer at xxx',
+      from: '01.2019',
+      frontTech: ['Javascript/Typescript', 'Angular', 'Ionic'],
+      frontTask: [
+        'building whole frontend layer of mobile apps single-handedly',
+        'communication with rest API',
+        'full-fledged participating in planning app architecture and data flow'],
+      backTech: ['Python'],
+      backTask: ['web-scrapping']
+    }
+  };
+
+  showCurrent() {
+    this.currentVisible ? this.currentVisible = false : this.currentVisible = true;
+  }
+
   ngOnInit() {
   }
 
