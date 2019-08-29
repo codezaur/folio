e526b328f7391a5baebbe498bbaf60aa6365dd39
DominikLipinski <maboonga@gmail.com>
2019-08-29 08:12:46 +0200

    added some cash projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,6 +9,23 @@ export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
+  cash = [
+    {name: 'Help Center in Wordpress',
+     desc: 'customisation of WP theme in line with provided graphical project',
+     live: 'https://help.reservis.pl/',
+     tech: ['ion-logo-css3', 'ion-logo-wordpress'],
+     pic: '../../assets/img/wpreservis.png'
+    },
+    {name: 'Landing Page for Real Estate Developer',
+     desc: 'Done in cooperation with other developer. PSD 2 HTML + pixel perfect. \
+     My parts were: attractions, example house, available houses sections, \
+     and backend for contact form.',
+    live: 'http://osiedlewilanowka.pl/',
+    tech: ['ion-logo-css3', 'ion-logo-javascript', 'ion-logo-sass'],
+    pic: '../../assets/img/wilanowka.png'
+   }
+  ];
+
   ngOnInit() {
   }
 
